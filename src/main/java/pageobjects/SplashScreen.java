0135b18f5187bafa08ee67b78d41c9e4e0e08aef
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-12-14 12:17:51 +0200

    Added UI elements in SplashScreen and method to click on Sign Up

--- src/main/java/pageobjects/SplashScreen.java
+++ src/main/java/pageobjects/SplashScreen.java
@@ -1,5 +1,36 @@
 package pageobjects;
 
+import org.openqa.selenium.By;
+
+import io.appium.java_client.android.AndroidDriver;
+
 public class SplashScreen {
+
+	// Driver declaration
+	AndroidDriver driver;
+
+	// UI elements on Spalsh Screen
+
+	// Login link
+	By loginLink = By.xpath("//android.widget.TextView[@text='Already Cambassador? Log In.']");
+	// Sign Up link
+	By signUpLink = By.xpath("//android.widget.TextView[@text='Become a Cambassador? Sign Up.']");
+	// image on the Splash Screen
+	By splashImage = By.id("com.cambassy:id/slide_image");
+
+	// constructor for SpalshScreen
+	public SplashScreen(AndroidDriver driver) {
+		this.driver = driver;
+	}
+
+	// method clicking on LogIn link
+	public void clickLogIn() {
+		driver.findElement(loginLink).click();
+	}
+
+	// method clicking on SignUp link
+	public void clickSignUp() {
+		driver.findElement(signUpLink).click();
+	}
 
 }
