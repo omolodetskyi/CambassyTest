895c000b18021bf70b4f3fb18e1967aaf6e004d4
Oleksandr Molodetskyi <molodetskyidev@gmail.com>
2017-12-15 23:46:53 +0200

    added aftertest to logintest

--- src/test/java/com/cambassy/cambassy_test/LogInTest.java
+++ src/test/java/com/cambassy/cambassy_test/LogInTest.java
@@ -4,19 +4,30 @@ import java.net.MalformedURLException;
 import java.util.concurrent.TimeUnit;
 
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import pageobjects.AnyScreen;
+import pageobjects.BottomMenu;
 import pageobjects.HomeScreen;
 import pageobjects.LoginScreen;
+import pageobjects.ProfileScreen;
+import pageobjects.SettingsScreen;
 import pageobjects.SplashScreen;
 import pageobjects.TestBase;
 
 public class LogInTest extends TestBase {
 	AndroidDriver<AndroidElement> driver;
+	SplashScreen splash = new SplashScreen(driver);
+	LoginScreen login = new LoginScreen(driver);
+	HomeScreen home = new HomeScreen(driver);
+	AnyScreen any = new AnyScreen(driver);
+	BottomMenu bottomMenu = new BottomMenu(driver);
+	SettingsScreen settings = new SettingsScreen(driver);
+	ProfileScreen profile = new ProfileScreen(driver);
 
 	@BeforeTest
 	public void openApp() throws MalformedURLException {
@@ -26,10 +37,7 @@ public class LogInTest extends TestBase {
 
 	@Test
 	public void LogIn_Test() {
-		SplashScreen splash = new SplashScreen(driver);
-		LoginScreen login = new LoginScreen(driver);
-		HomeScreen home = new HomeScreen(driver);
-		AnyScreen any = new AnyScreen(driver);
+
 		// click on Login link
 		splash.clickLogIn();
 		// check if Login screen is opened
@@ -48,4 +56,14 @@ public class LogInTest extends TestBase {
 		Assert.assertTrue(home.checkTitle(), "Can not find title on home page");
 	}
 
+	@AfterTest
+	public void tearDown() {
+		// Go to Profile
+		bottomMenu.clickProfile();
+		// Go to Settings
+		profile.clickSettingsBtn();
+		// Click log out button
+		settings.clickLogout();
+		driver.quit();
+	}
 }
